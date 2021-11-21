3bfd1241e8fc83c560f709c42f6838e0ecb36fdc
XenioxYT <tomlockwood135@outlook.com>
2021-11-21 00:16:36 +0000

    Updated UI for main activity

--- app/src/main/java/com/example/instantmessagingapp/MainActivity.kt
+++ app/src/main/java/com/example/instantmessagingapp/MainActivity.kt
@@ -2,10 +2,25 @@ package com.example.instantmessagingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            Log.d("MainActivity", "Button clicked")
+            val email = email_editText_register.toString()
+            val password = password_editText_register.toString()
+            val confirmPassword = confirm_password_editText_register.toString()
+            val username = username_editText_register.toString()
+
+            Log.d("MainActivity", "Email is $email")
+            Log.d("MainActivity", "Password is $password")
+            Log.d("MainActivity", "Confirm Password is $confirmPassword")
+            Log.d("MainActivity", "Username is $username")
+        }
     }
 }
