adf7685bce8c3822a9201a5eb517e44829d5e13b
XenioxYT <tomlockwood135@outlook.com>
2021-11-23 09:01:18 +0000

    added button layout

--- app/src/main/java/com/example/instantmessagingapp/LoginActivity.kt
+++ app/src/main/java/com/example/instantmessagingapp/LoginActivity.kt
@@ -1,11 +1,23 @@
 package com.example.instantmessagingapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.login_activity.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login_activity)
+
+        // Set up the login form.
+        button_login_loginActivity.setOnClickListener {
+            Log.d("LoginActivity", "Login button pressed")
+             // Check if text fields are empty
+            if (username_editText_login.text.toString().isEmpty() || password_editText_login.text.toString().isEmpty()) {
+                Log.d("LoginActivity", "Login failed")
+                return@setOnClickListener
+        }
     }
 }
