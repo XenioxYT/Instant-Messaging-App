ccfa032781ddfc2ce8c2afcfbe2350cd6ceece25
xeniox69 <tomlockwood135@outlook.com>
2021-11-27 20:18:43 +0000

    feat: moved input checking to functions

--- app/src/main/java/com/example/instantmessagingapp/SignupActivity.kt
+++ app/src/main/java/com/example/instantmessagingapp/SignupActivity.kt
@@ -8,11 +8,10 @@ import android.text.SpannableString
 import android.text.style.AlignmentSpan
 import android.util.Log
 import android.util.Patterns
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.gms.tasks.Task
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuthInvalidUserException
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -25,6 +24,12 @@ class SignupActivity : AppCompatActivity() {
         // Listen for the create account button press
         button_create_account.setOnClickListener {
             Log.d("SignupActivity", "Button clicked")
+
+            checkUsername()
+            checkEmail()
+            checkPassword()
+            checkPasswordsMatch()
+
             val username =
                 username_editText_register.text.toString() // Set username to the text entered into the username text box
             val email =
@@ -34,67 +39,17 @@ class SignupActivity : AppCompatActivity() {
             val confirmPassword =
                 confirm_password_editText_register.text.toString() // Set confirmPassword to the text entered into the confirm_password text box
 
-
-            // Check if the username is empty
-            if (username_editText_register.text.isNullOrEmpty()) {
-                username_editText_register.error = "Username Required"
-                username_editText_register.requestFocus()
-            }
-            // Check if the email is empty
-            if (email_editText_register.text.isNullOrEmpty()) {
-                email_editText_register.error = "Email is required"
-                email_editText_register.requestFocus()
-            }
-            // Check if the password is empty
-            if (password_editText_register.text.isNullOrEmpty()) {
-                password_editText_register.error = "Password is required"
-                password_editText_register.requestFocus()
-            }
-            // Check if the confirm password is empty
-            if (confirm_password_editText_register.text.isNullOrEmpty()) {
-                confirm_password_editText_register.error = "Confirm Password is required"
-                confirm_password_editText_register.requestFocus()
-            }
-            // if all the text boxes are filled in, then check if the password and confirm password match
-            if (password != confirmPassword) {
-                confirm_password_editText_register.error = "Passwords do not match"
-                confirm_password_editText_register.requestFocus()
-            }
-            // If all the text boxes are filled in, then check if the email is valid
-            if (!email_editText_register.text.isNullOrEmpty()) {
-                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                    email_editText_register.error = "Email is invalid"
-                    email_editText_register.requestFocus()
-                }
-            }
-            // If all the text boxes are filled in, then check if the password is valid
-            if (!password_editText_register.text.isNullOrEmpty()) {
-                if (password.length < 6) {
-                    password_editText_register.error = "Password must be at least 6 characters"
-                    password_editText_register.requestFocus()
-                }
-            }
-            // If all the text boxes are filled in, then check if the username is valid
-            if (!username_editText_register.text.isNullOrEmpty()) {
-                if (username.length <= 6 || username.length >= 18) {
-                    username_editText_register.error = "Username must be at least 6 characters and no more than 18 characters"
-                    username_editText_register.requestFocus()
-                }
-            }
-
             // I have no idea why this code works, but it does
             // However, something may break cos it's shit
             if (username_editText_register.text.isNotEmpty() && email_editText_register.text.isNotEmpty() && password_editText_register.text.isNotEmpty() && confirm_password_editText_register.text.isNotEmpty() && password == confirmPassword && Patterns.EMAIL_ADDRESS.matcher(email).matches() && password.length >= 6 && username.length <= 18) {
                 try {
                     FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                         .addOnCompleteListener {
-                            if (!it.isSuccessful) return@addOnCompleteListener
-                            Log.d(
-                                "SignupActivity",
-                                "Successfully created user with uid: ${it.result?.user?.uid}"
-                            )
-                            val intent = Intent(this, LoginActivity::class.java)
-                            startActivity(intent)
+//                            if (!it.isSuccessful) {
+//                                return@addOnCompleteListener
+//                            }
+//                            Log.d("SignupActivity", "Successfully created user with uid: ${it.result?.user?.uid}")
+                            loginSuccessfulRegister(it)
                         }
                         .addOnFailureListener {
                             Log.d("SignupActivity", "Failed to create user: ${it.message}")
@@ -130,7 +85,7 @@ class SignupActivity : AppCompatActivity() {
                     val builder = AlertDialog.Builder(context)
                     builder.setTitle(title)
                     builder.setMessage("There was an error creating your account. Please try again in a few minutes.")
-                    builder.setPositiveButton("OK") { dialog, which ->
+                    builder.setPositiveButton("OK") { dialog, _ ->
                         dialog.dismiss()
                     }
                     val dialog: AlertDialog = builder.create()
@@ -170,6 +125,78 @@ class SignupActivity : AppCompatActivity() {
                 startActivity(it)
             }
         }
+    }
+
+
+    private fun checkUsername() {
+        if (!username_editText_register.text.isNullOrEmpty()) {
+            if (username_editText_register.text.toString().length <= 6 || username_editText_register.text.toString().length >= 18) {
+                username_editText_register.error =
+                    "Username must be at least 6 characters and no more than 18 characters"
+                username_editText_register.requestFocus()
+                return
+            }
+        }
+        else {
+            username_editText_register.error = "Username cannot be empty"
+            username_editText_register.requestFocus()
+            return
+        }
+    }
+
+    private fun checkEmail() {
+        if (!email_editText_register.text.isNullOrEmpty()) {
+            if (!Patterns.EMAIL_ADDRESS.matcher(email_editText_register.text.toString()).matches()) {
+                email_editText_register.error = "Email is invalid"
+                email_editText_register.requestFocus()
+                return
+            }
+        }
+        else {
+            email_editText_register.error = "Email is required"
+            email_editText_register.requestFocus()
+            return
+        }
+    }
+
+    private fun checkPassword() {
+        if (!password_editText_register.text.isNullOrEmpty()) {
+            if (password_editText_register.text.toString().length < 6) {
+                password_editText_register.error = "Password must be at least 6 characters"
+                password_editText_register.requestFocus()
+                return
+            }
+        }
+        else {
+            password_editText_register.error = "Password cannot be empty"
+            password_editText_register.requestFocus()
+            return
+        }
+    }
+
+    private fun checkPasswordsMatch() {
+        if (!confirm_password_editText_register.text.isNullOrEmpty()) {
+            if (confirm_password_editText_register.text.toString() != password_editText_register.text.toString()) {
+                confirm_password_editText_register.error = "Passwords do not match"
+                confirm_password_editText_register.requestFocus()
+                return
+            }
+        }
+        else {
+            confirm_password_editText_register.error = "Confirm Password cannot be empty"
+            confirm_password_editText_register.requestFocus()
+            return
+        }
+    }
+
+    private fun loginSuccessfulRegister(it: Task<AuthResult>) {
+        if (!it.isSuccessful) return
+        Log.d(
+            "SignupActivity",
+            "Successfully created user with uid: ${it.result?.user?.uid}"
+        )
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
     }
 }
 
