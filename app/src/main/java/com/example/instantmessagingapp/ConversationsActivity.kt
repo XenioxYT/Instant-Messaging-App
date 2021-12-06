1168b8c881a6100de371df4d4556fe5c1e8c8df2
xeniox69 <tomlockwood135@outlook.com>
2021-12-06 23:21:56 +0000

    feat: added navigation drawer. check activity_conversations.xml and ConversationsActivity.kt for changes.

--- app/src/main/java/com/example/instantmessagingapp/ConversationsActivity.kt
+++ app/src/main/java/com/example/instantmessagingapp/ConversationsActivity.kt
@@ -3,23 +3,54 @@ package com.example.instantmessagingapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import kotlinx.android.synthetic.main.activity_conversations.*
 
-
 class ConversationsActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_conversations)
 
-        setNavigationViewListener()
-
-    }
-
-    private fun setNavigationViewListener() {
-        val navigationView = findViewById<androidx.appcompat.widget.Toolbar>(R.id.topAppBar)
-        navigationView.setNavigationOnClickListener {
-            navigationView.open()
+        topAppBar.setNavigationOnClickListener {
+            drawer_layout.openDrawer(GravityCompat.START)
         }
 
+        toggle = ActionBarDrawerToggle(this, drawer_layout, R.string.open, R.string.close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        actionBar?.setDisplayHomeAsUpEnabled(true)
+
+        navigation_drawer.setNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.item2 -> {
+                    val intent = Intent(this, SignupActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.item3 -> {
+                    val intent = Intent(this, ConversationsActivity::class.java)
+                    startActivity(intent)
+                }
+                R.id.item4 -> {
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            drawer_layout.closeDrawer(navigation_drawer)
+            true
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        if (toggle.onOptionsItemSelected(item)) {
+            return true
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
