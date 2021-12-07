7091d77712e8ef6d7048a618439e51d0e5be95d9
xeniox69 <tomlockwood135@outlook.com>
2021-12-07 21:53:24 +0000

    chore: added comments

--- app/src/main/java/com/example/instantmessagingapp/LoginAccountCreatedActivity.kt
+++ app/src/main/java/com/example/instantmessagingapp/LoginAccountCreatedActivity.kt
@@ -12,29 +12,29 @@ class LoginAccountCreatedActivity : AppCompatActivity() {
 
         // Listen for the login button press
         button_login_loginActivity.setOnClickListener {
-            Log.d("LoginActivity", "Login button pressed")
-            // Check if username is empty
-            if (username_editText_login.text.toString().isEmpty()) {
-                username_editText_login.error = "Username cannot be empty"
+            Log.d("LoginActivity", "Login button pressed") // Logs the button press to the console
+            // Check if username is empty or null
+            if (username_editText_login.text.toString().isEmpty()) { // If username is empty
+                username_editText_login.error = "Username cannot be empty" // Set error message
             }
             // Check if password is empty
-            if (password_editText_login.text.toString().isEmpty()) {
-                password_editText_login.error = "Password cannot be empty"
+            if (password_editText_login.text.toString().isEmpty()) { // If password is empty
+                password_editText_login.error = "Password cannot be empty" // Set error message
             }
             if (username_editText_login.text.toString()
                     .isNotEmpty() && password_editText_login.text.toString().isNotEmpty()
-            ) {
+            ) { // If username and password are not empty
                 //TODO: Login the user once details have been validated
             }
         }
 
         // Listen for the back to sign in button press
         button_back_to_signin.setOnClickListener {
-            Log.d("LoginActivity", "Back to sign in button pressed")
+            Log.d("LoginActivity", "Back to sign in button pressed") // Logs the button press to the console
 
             // Clear error messages
-            username_editText_login.error = null
-            password_editText_login.error = null
+            username_editText_login.error = null // Clear error message
+            password_editText_login.error = null // Clear error message
 
             //End the activity
             finish()
