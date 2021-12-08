e38f94f482711064da5df681c42110a42e4bc95f
XenioxYT <tomlockwood135@outlook.com>
2021-12-08 12:18:08 +0000

    feat: added SettingsActivity.kt and activity_settings.xml. Also added actionbar_menu.xml to allow other options within the action bar such as search.

---
+++ app/src/main/java/com/example/instantmessagingapp/SettingsActivity.kt
@@ -0,0 +1,58 @@
+package com.example.instantmessagingapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ActionMenuView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
+import com.google.android.material.appbar.MaterialToolbar
+import kotlinx.android.synthetic.main.activity_conversations.*
+
+class SettingsActivity : AppCompatActivity() {
+
+    lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState) // call super class onCreate method
+        setContentView(R.layout.activity_settings) // set the layout of the activity
+
+        topAppBar.setNavigationOnClickListener { // set the navigation icon on the top app bar
+            drawer_layout.openDrawer(GravityCompat.START) // open the drawer
+        }
+
+        toggle = ActionBarDrawerToggle(this, drawer_layout, R.string.open, R.string.close) // create a toggle for the drawer
+        drawer_layout.addDrawerListener(toggle) // add the toggle to the drawer layout
+        toggle.syncState() // sync the toggle state
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) // enable the back button on the top app bar
+
+        navigation_drawer.setNavigationItemSelectedListener { // Set the navigation click listener
+            when(it.itemId){ // check which item was clicked
+                R.id.conversations -> { // if the item was the second item
+                    val intent = Intent(this, ConversationsActivity::class.java) // create an intent to go to the signup activity
+                    startActivity(intent) // start the intent
+                }
+                R.id.settings -> { // if the item was the third item
+                    val intent = Intent(this, SettingsActivity::class.java) // create an intent to go to the conversations activity
+                    startActivity(intent) // start the intent
+                }
+                R.id.logout -> { // if the item was the fourth item
+                    val intent = Intent(this, LoginActivity::class.java) // create an intent to go to the login activity
+                    startActivity(intent) // start the intent
+                }
+            }
+            //drawer_layout.closeDrawer(navigation_drawer) // close the drawer
+            true // return true to indicate that the item was selected
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean { // override the onOptionsItemSelected method
+
+        if (toggle.onOptionsItemSelected(item)) { // if the toggle was selected
+            return true // return true
+        }
+        return super.onOptionsItemSelected(item) // return the super class onOptionsItemSelected method
+    }
+}
