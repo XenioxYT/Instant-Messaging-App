a865e8777e01ed0b43160975e7f30622f9d82cf5
XenioxYT <tomlockwood135@outlook.com>
2022-02-25 14:28:06 +0000

    feat: actually fixed the app

---
+++ app/src/main/java/com/xeniox/instantmessagingapp/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.xeniox.instantmessagingapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.xeniox.instantmessagingapp.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
