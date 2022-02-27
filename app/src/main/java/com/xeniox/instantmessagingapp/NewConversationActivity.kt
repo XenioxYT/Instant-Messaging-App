6ff9c226be5be972aafd2d66636c65a6dc8118e3
xeniox69 <tomlockwood135@outlook.com>
2022-02-27 21:01:49 +0000

    groupie view adapter scrolling with user_row_new_conversation.xml

--- app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
@@ -2,9 +2,9 @@ package com.xeniox.instantmessagingapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.xeniox.instantmessagingapp.R
 import com.xwray.groupie.GroupAdapter
-import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_conversation.*
 
 class NewConversationActivity : AppCompatActivity() {
@@ -12,12 +12,29 @@ class NewConversationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_conversation)
 
-        val adapter = GroupAdapter<GroupieViewHolder>()
+        val adapter = GroupAdapter<ViewHolder>()
+
+        adapter.add(UserItem())// add the layout.
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+
         recyclerView_new_conversation.adapter = adapter
+
 
 
         topAppBar_new_conversation.setNavigationOnClickListener { // set the navigation icon on the top app bar
             finish() // end the activity
         }
+    }
+}
+
+class UserItem : Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        // will be called in our list for each user object later on...
+    }
+
+    override fun getLayout(): Int {
+        return R.layout.user_row_new_conversation
     }
 }
