aa0cab46bcf65f365ea0617df554d4d64d460316
xeniox69 <tomlockwood135@outlook.com>
2022-02-28 23:55:56 +0000

    added images

--- app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
@@ -7,6 +7,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
@@ -52,6 +53,7 @@ class NewConversationActivity : AppCompatActivity() {
 class UserItem(val user: User) : Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
         viewHolder.itemView.text_username_new_conversation.text = user.username
+        Picasso.get().load(user.profileImageUrl).into(viewHolder.itemView.imageView)
     }
 
     override fun getLayout(): Int {
