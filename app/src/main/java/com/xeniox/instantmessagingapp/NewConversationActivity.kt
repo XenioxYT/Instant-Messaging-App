e7b0ece15416ac5a0b1b37cfa42900749259dc0a
xeniox69 <tomlockwood135@outlook.com>
2022-02-28 21:52:48 +0000

    displayed all of the usernames of all the users in the database in the contacts screen (however the user still cannot click them or select them)

--- app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/NewConversationActivity.kt
@@ -1,48 +1,57 @@
 package com.xeniox.instantmessagingapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
 import kotlinx.android.synthetic.main.activity_new_conversation.*
+import kotlinx.android.synthetic.main.user_row_new_conversation.view.*
 
 class NewConversationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_conversation)
 
-        val adapter = GroupAdapter<ViewHolder>()
-
-        adapter.add(UserItem())// add the layout.
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-
-
-        recyclerView_new_conversation.adapter = adapter
-
-
-
         topAppBar_new_conversation.setNavigationOnClickListener { // set the navigation icon on the top app bar
             finish() // end the activity
         }
+        fetchUsers()
     }
+
+    private fun fetchUsers() {
+        val ref = FirebaseDatabase.getInstance().getReference("/users")
+        ref.addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+
+                val adapter = GroupAdapter<ViewHolder>()
+                p0.children.forEach {
+                    Log.d("NewMessage", it.toString())
+                    val user = it.getValue(User::class.java)
+                    if (user != null) {
+                        adapter.add(UserItem(user))
+                    }
+                }
+                recyclerView_new_conversation.adapter = adapter
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+    }
+
 }
 
-class UserItem : Item<ViewHolder>() {
+class UserItem(val user: User) : Item<ViewHolder>() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        // will be called in our list for each user object later on...
+        viewHolder.itemView.text_username_new_conversation.text = user.username
     }
 
     override fun getLayout(): Int {
