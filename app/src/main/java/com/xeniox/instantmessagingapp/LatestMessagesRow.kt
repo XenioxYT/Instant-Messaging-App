fc76f6d483c9a63266b64e47d51f783190ea4fbb
Xeniox <tomlockwood135@outlook.com>
2022-03-05 21:46:08 +0000

    Added horizontal bars separating the recent conversations, and moved LatestMessagesRow.kt class into a new file

---
+++ app/src/main/java/com/xeniox/instantmessagingapp/LatestMessagesRow.kt
@@ -0,0 +1,46 @@
+package com.xeniox.instantmessagingapp
+
+import android.util.Log
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
+import com.xwray.groupie.Item
+import com.xwray.groupie.ViewHolder
+import kotlinx.android.synthetic.main.conversations_message_row.view.*
+
+class LatestMessageRow(val chatMessage: ChatMessage): Item<ViewHolder>() {
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.itemView.conversations_message_row_textView.text = chatMessage.text
+
+        val chatPartnerId = if (chatMessage.fromId == FirebaseAuth.getInstance().uid) {
+            chatMessage.toId
+        } else {
+            chatMessage.fromId
+        }
+        val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+        ref.addListenerForSingleValueEvent(object: ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val user = snapshot.getValue(User::class.java)
+                viewHolder.itemView.conversations_message_username.text = user?.username
+                val uri = user?.profileImageUrl
+                Log.d("LatestMessageRow", "uri: $uri")
+                if (uri != null) {
+                    val targetImageView = viewHolder.itemView.user_profile_picture_conversations_message_row
+                    Picasso.get().load(uri).into(targetImageView)
+                } else {
+                    val targetImageView = viewHolder.itemView.user_profile_picture_conversations_message_row
+                    Picasso.get().load(R.drawable.ic_account_circle_black_24dp).into(targetImageView)
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+            }
+        })
+    }
+    override fun getLayout(): Int {
+        return R.layout.conversations_message_row
+    }
+
+}
