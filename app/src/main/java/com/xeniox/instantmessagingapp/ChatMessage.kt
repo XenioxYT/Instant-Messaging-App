56f40e822766d836c41b8c914af4a0ea752f050d
XenioxYT <tomlockwood135@outlook.com>
2022-03-18 15:04:43 +0000

    Cleaned unused variables.

--- app/src/main/java/com/xeniox/instantmessagingapp/ChatMessage.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/ChatMessage.kt
@@ -1,9 +1,3 @@
 package com.xeniox.instantmessagingapp
 
-import com.google.firebase.analytics.FirebaseAnalytics
-
-private lateinit var firebaseAnalytics: FirebaseAnalytics
-class ChatMessage(val id: String, val text:String, val fromId: String, val toId: String, val timestamp: Long) {
-
-    constructor() : this("", "", "", "", -1)
-}
+class ChatMessage(val id: String, val text:String, val fromId: String, val toId: String, val timestamp: Long)
