58407388359793bff52ecddfb4746c2184cae135
Xeniox <tomlockwood135@outlook.com>
2022-03-20 15:29:59 +0000

    User status

--- app/src/main/java/com/xeniox/instantmessagingapp/LoginActivity.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/LoginActivity.kt
@@ -86,7 +86,7 @@ class LoginActivity : AppCompatActivity() {
                         dialog.dismiss()
                         val uid = FirebaseAuth.getInstance().uid
                         val email = FirebaseAuth.getInstance().currentUser?.email
-                        User(uid.toString(), "", "", email.toString(),"", "","null")
+                        User(uid.toString(), "", "", email.toString(),"", "","null","null","")
                         if (!it.isSuccessful) return@addOnCompleteListener
 
                         Log.d(
