2bbae627464ec68703e80814a371b914da62d75a
Xeniox <tomlockwood135@outlook.com>
2022-03-20 15:30:10 +0000

    User status

--- app/src/main/java/com/xeniox/instantmessagingapp/ChatMessage.kt
+++ app/src/main/java/com/xeniox/instantmessagingapp/ChatMessage.kt
@@ -1,5 +1,12 @@
 package com.xeniox.instantmessagingapp
 
-class ChatMessage(val id: String, val text:String, val fromId: String, val toId: String, val timestamp: Long) {
-    constructor() : this("", "", "", "", -1)
+class ChatMessage(
+    val id: String,
+    val text: String,
+    val fromId: String,
+    val toId: String,
+    val timestamp: Long,
+    val fromUsername: String,
+) {
+    constructor() : this("", "", "", "", -1, "")
 }
